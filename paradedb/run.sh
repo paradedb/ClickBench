26aa0b9d89185aa956063f889e983cb4eaee3a33
Philippe Noël <philippemnoel@gmail.com>
2024-05-20 10:21:50 -0400

    Try this

--- paradedb/run.sh
+++ paradedb/run.sh
@@ -9,6 +9,6 @@ cat queries.sql | while read query; do
 
     echo "$query";
     for i in $(seq 1 $TRIES); do
-        psql -U postgres -t -c '\timing' -c "$query" | grep 'Time'
+        psql -t -c '\timing' -c "$query" | grep 'Time'
     done;
 done;
